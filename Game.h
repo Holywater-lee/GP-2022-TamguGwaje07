6102888f90c4227fa4d5b4e1ac9f13aaf069d03b
Holeewater <83389425+Holywater-lee@users.noreply.github.com>
2022-04-29 04:38:45 +0000

    Initial commit

---
+++ Game.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "usr/include/SDL2/SDL.h"
+#include "usr/include/SDL2/SDL2_gfxPrimitives.h"
+#include "usr/include/SDL2/SDL_image.h"
+
+#include <vector>
+
+
+class Walker;
+#include "Vector2D.h"
+
+class Game {
+private:
+    Game() : target(0, 0) {}
+    static Game* s_pInstance; // 정적 멤버변수 
+public:
+    static Game* Instance() {
+   	  if (s_pInstance == 0) {
+        s_pInstance = new Game();
+        return s_pInstance;
+    	}
+    	return s_pInstance;
+    }
+
+    SDL_Renderer* getRenderer() const { return m_pRenderer; }
+
+public:
+    ~Game() {}
+
+    bool init(const char *title, int xpos, int ypos, int width, int height, int flags);
+    bool setup();
+    void render();
+    void update(); 
+    bool running();
+    void handleEvents();
+    void clean();
+    void quit() { m_bRunning = false; }
+
+private:
+    SDL_Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
+    bool m_bRunning;
+
+    Walker* _walker;
+    Vector2D target;
+};
+
+typedef Game TheGame;
---
+++ Walker.cpp
@@ -0,0 +1,53 @@
+#include "Walker.h"
+
+const int WIDTH = 600;
+const int HEIGHT = 400;
+
+Walker::Walker(float x, float y) : location(x, y), velocity(0, 0), acceleration(0, 0), r(16), maxForce(0.25), maxSpeed(4)
+{
+
+}
+
+void Walker::update() 
+{
+  velocity += acceleration;
+  velocity.limit(maxSpeed);
+  location += velocity;
+  acceleration *= 0;
+
+  heading = atan2(velocity.getY(), velocity.getX());
+}
+
+void Walker::seek(Vector2D target)
+{
+  Vector2D desiredForce = target - location;
+  desiredForce.normalize();
+  desiredForce *= maxSpeed;
+  desiredForce -= velocity;
+  desiredForce.limit(maxForce);
+  applyForce(desiredForce);
+}
+
+void Walker::applyForce(Vector2D force)
+{
+  acceleration += force;
+}
+
+void Walker::edges()
+{
+  float buffer = r * 2;
+  if (location.getX() > WIDTH + buffer) location.setX(-buffer);
+  else if (location.getX() < -buffer) location.setX(WIDTH + buffer);
+  if (location.getY() > HEIGHT + buffer) location.setY(-buffer);
+  else if (location.getY() < -buffer) location.setY(HEIGHT + buffer);
+}
+
+void Walker::draw(SDL_Renderer* renderer)
+{
+  Vector2D vertex0 = Vector2D(r, 0).RotTranslate(heading, location.getX(), location.getY());
+  Vector2D vertex1 = Vector2D(-r, -r / 2).RotTranslate(heading, location.getX(), location.getY());
+  Vector2D vertex2 = Vector2D(-r, r / 2).RotTranslate(heading, location.getX(), location.getY());
+  
+  filledTrigonRGBA(renderer, vertex0.getX(), vertex0.getY(), vertex1.getX(), vertex1.getY(), vertex2.getX(), vertex2.getY(), 125, 125, 125, 255);
+  aatrigonRGBA(renderer, vertex0.getX(), vertex0.getY(), vertex1.getX(), vertex1.getY(), vertex2.getX(), vertex2.getY(), 255, 255, 255, 255);
+}
